2c8c290e84b0e77158df23da6b9a8102f64ffbf1
unnimayat <88229921+unnimayat@users.noreply.github.com>
2023-09-09 12:06:41 +0530

    first

---
+++ src/components/Home/Home.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./Home.css"
+
+export default function Home(){
+  return (
+    <div className='main-body'>
+       
+       <div className='sidebar'>
+             
+       </div>
+       <div className='right'>
+            <div className='top'>
+    
+            </div>
+            <div className='break'></div>
+            <div className='bottom'>
+                <div>
+
+                </div>
+                
+            </div>
+       </div>
+    </div>
+  );
+}
