2d114b057967ad1a5cc3c7854cc73b33626786c3
pmdevika <98165952+pmdevika@users.noreply.github.com>
2023-09-22 21:51:02 +0530

    Update Summary_qgrading.js

--- src/components/Summary/Summary_qgrading.js
+++ src/components/Summary/Summary_qgrading.js
@@ -1,179 +1,132 @@
 import React, { useState, useEffect } from 'react';
 import './Summary_qgrading.css';
-import userImage from '../../assets/user_circle.png'; // Import the image
 import logoImage from '../../assets/shg.png';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-
 export default function Grading() {
-  const [name, setName] = useState('');
-  const [position, setPosition] = useState('');
-  const [date, setDate] = useState('');
-  const [period, setPeriod] = useState('');
-  const [review, setReview] = useState('');
-  const [evaluation, setEvaluation] = useState('');
+  const [tableData, setTableData] = useState({
+    knowledgeParameters: { maxScore: '', selfScore: '', evaluatorScore: '', reviewerScore: '', finalScore: '', comments: '' },
+    professionalIntegrity: { maxScore: '', selfScore: '', evaluatorScore: '', reviewerScore: '', finalScore: '', comments: '' },
+    responsibilityFulfillment: { maxScore: '', selfScore: '', evaluatorScore: '', reviewerScore: '', finalScore: '', comments: '' },
+  });
 
-  const [tableData, setTableData] = useState([
-    { subject: '', grade: '', internalScore: '', externalScore: '' },
-  ]);
-  const [id, setId] = useState('');
-
-  const navigate = useNavigate();
-
-  const token = localStorage.getItem('token');
-  const role = localStorage.getItem('role');
-  const isReviewer = role !== 'reviewer';
-  
   useEffect(() => {
-    // Retrieve the token, ID, and role from local storage
+    // Retrieve the token and ID from local storage
     const token = localStorage.getItem('token');
     const ID = localStorage.getItem('ID');
     const role = localStorage.getItem('role');
 
     // Set the default Authorization header for Axios
     axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-    setId(ID);
-  }, []);
 
-  // Function to add a new row
-  const addRow = () => {
-    setTableData([...tableData, { subject: '', grade: '', internalScore: '', externalScore: '' }]);
+    // Fetch recommendations data based on appraiseeId
+    if (role=== 'reviewer') {
+      axios
+        .get(`https://appbackend-rala.onrender.com/summary/get-table-data/${"64fd8e3b9a14a681cba43ad3"}`)
+        .then((response) => {
+          const data = response.data;
+
+          // Update the state with the fetched data for each category
+          setTableData((prevTableData) => ({
+            ...prevTableData,
+            knowledgeParameters: data.knowledgeParameters,
+            professionalIntegrity: data.professionalIntegrity,
+            responsibilityFulfillment: data.responsibilityFulfillment,
+          }));
+
+          console.log('Fetched Data:', data);
+        })
+        .catch((error) => {
+          console.error('Error fetching data:', error);
+        });
+    }
+  }, []);
+  const navigate = useNavigate();
+
+  const handleSubmit = () => {
+    // Implement your submission logic here
+    // You can send the data to your backend or perform any other necessary actions
+    console.log('Submit button clicked');
+    navigate("/feedback")
   };
-  const handleSave = () => {
-    // Handle saving the data here (you can send a request to your API)
-    // For example, you can send a POST request to your server to save the feedback.
-    // You can also include strengthInput1 and strengthInput2 in the request body.
-    
-    console.log('');
-    
-    // Navigate to the recommendation page
-    navigate('/feedback');
+
+  // Function to handle the "Next" button click
+  
+
+  // Function to handle input changes in the "comments" field
+  const handleCommentsChange = (category, value) => {
+    setTableData((prevTableData) => ({
+      ...prevTableData,
+      [category]: {
+        ...prevTableData[category],
+        comments: value,
+      },
+    }));
   };
-  const handlenavigate = () => {
-    // Handle saving the data here (you can send a request to your API)
-    // For example, you can send a POST request to your server to save the feedback.
-    // You can also include strengthInput1 and strengthInput2 in the request body.
-    
-    console.log('');
-    
-    // Navigate to the recommendation page
-    navigate('/feedback');
-  };
+
 
   return (
     <div className="main-body">
       <div className="sidebar">
         {/* Sidebar content */}
-        <img src={logoImage} alt="Example" className='logoimage' />
-        <div className="sidebar-item" style={{ marginTop: 50 }}>
-          <i className="material-icons"></i>
-          <span>Dashboard</span>
-        </div>
-        <div className="sidebar-item">
-          <i className="material-icons"></i>
-          <span>Self Appraisal</span>
-        </div>
-        <div className="sidebar-item">
-          <i className="material-icons"></i>
-          <span>Team</span>
-        </div>
+        <img src={logoImage} alt="Example" className="logoimage" />
+        {/* ... (rest of your sidebar) ... */}
       </div>
 
       <div className="right">
         <div className="top">
           {/* Display the image */}
-          <h1 className='name' style={{ marginRight: 600, marginTop: 30 }}>Summary of Quantitative grading</h1>
-          <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-end', marginRight: '100px' }}>
-            <img src={userImage} alt="Example" className='profileimage' />
-
-            {/* Display the name and id */}
-            <div style={{ display: 'flex', flexDirection: 'column' }}>
-              <h3 className="name">{id}</h3>
-              <p className="name" style={{ fontWeight: 300, fontSize: 16, marginTop: -15 }}>
-                {role}
-              </p>
-            </div>
-          </div>
+          <h1 className="name" style={{ marginRight: 600, marginTop: 30 }}>
+            Acceptance
+          </h1>
+          {/* ... (rest of your top section) ... */}
         </div>
         <div className="break"></div>
         <div className="bottom">
           <div className="profile-page">
-            {/* Insert the table here */}
             <table>
               <thead>
                 <tr>
-                  <th className='smallbox2'>Category</th>
-                  <th className='smallbox2'>Maximum Score</th>
-                  <th className='smallbox2'>Achieved Score
-                  <th className='box1'>Self</th>
-                  <th className='box2'>Evaluation</th>
-                  <th className='box2'>Review</th></th>
-                  
-                  <th className='smallbox2'>Final score assigned</th>
-                  <th className='smallbox1'>Comments, if any</th>
-                </tr>
-                <tr>
-                  <th className='smallbox2'>Professional Integrity and Team Contribution parameters</th>
-                  <th className='smallbox'>50</th>
-                  <th className='smallbox2'>
-                  <th className='box2'></th>
-                  <th className='box2'></th>
-                  <th className='box2'></th></th>
-                  <th className='smallbox'></th>
-                  <th className='smallbox'></th>
-                  
-                </tr>
-                <tr>
-                  <th className='smallbox2'>Knowledge parameters</th>
-                  <th className='smallbox'>50</th>
-                  <th className='smallbox2'>
-                  <th className='box2'></th>
-                  <th className='box2'></th>
-                  <th className='box2'></th></th>
-                  <th className='smallbox'></th>
-                  <th className='smallbox'></th>
-                  
-                </tr>
-                <tr>
-                  <th className='smallbox2'>Responsibility Fulfilment parameters</th>
-                  <th className='smallbox'>100</th>
-                  <th className='smallbox2'>
-                  <th className='box2'></th>
-                  <th className='box2'></th>
-                  <th className='box2'></th></th>
-                  <th className='smallbox'></th>
-                  <th className='smallbox'></th>
-                  
+                  <th className="smallbox2">Category</th>
+                  <th className="smallbox2">Maximum Score</th>
+                  <th className="smallbox2">Self Score</th>
+                  <th className="smallbox2">Evaluator Score</th>
+                  <th className="smallbox2">Reviewer Score</th>
+                  <th className="smallbox2">Final Score</th>
+                  <th className="smallbox2">Comments</th>
                 </tr>
               </thead>
-              {/* Add table content here */}
-              
+              <tbody>
+                {Object.keys(tableData).map((category) => (
+                  <tr key={category}>
+                    <td className="smallbox">{category}</td>
+                    <td className="smallbox">{tableData[category].maxScore}</td>
+                    <td className="smallbox">{tableData[category].selfScore}</td>
+                    <td className="smallbox">{tableData[category].evaluatorScore}</td>
+                    <td className="smallbox">{tableData[category].reviewerScore}</td>
+                    <td className="smallbox">{tableData[category].finalScore}</td>
+                    <td className="smallbox">
+                <input
+                  type="text"
+                  value={tableData[category].comments}
+                  onChange={(e) => handleCommentsChange(category, e.target.value)}
+                />
+              </td>
+                  </tr>
+                ))}
+              </tbody>
             </table>
-            
-          
-     
-            
+
             <div>
               {/* Your other content */}
             </div>
 
             <div className="profile-section">
-            <button
-                type="submit"
-                disabled={isReviewer}
-                style={{ width: '10vw' }}
-                onClick={handleSave}
-              >
-                Submit
-              </button>
-              <button
-              style={{ width: '10vw' }}
-                type="submit"
-                onClick={handlenavigate}
-              >
-                Next
-              </button>
+            <button type="submit" onClick={handleSubmit}>
+          Submit
+        </button>
+        
             </div>
           </div>
         </div>
